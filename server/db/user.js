6526a955d407fcd0ea141f0ca6b6ede8af94bd45
james1q2w3e <james1q2w3e@gmail.com>
2022-10-30 14:28:25 -0400

    ADD authentication to User model

--- server/db/user.js
+++ server/db/user.js
@@ -1,11 +1,16 @@
 const Sequelize = require('sequelize');
 const db = require('./database');
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt');
+const axios = require('axios');
+
+const SALT_ROUNDS = 5;
 
 
 // add a "karma" in here for the game?
 
 // user model
-module.exports = db.define('user', {
+const User = db.define('user', {
   firstName: {
     type: Sequelize.STRING,
     allowNull: false,
@@ -46,3 +51,48 @@ module.exports = db.define('user', {
   },
 });
 
+module.exports = User;
+
+
+// instanceMethods
+User.prototype.correctPassword = function(candidatePwd) {
+  // we need to compare the plain version to an encrypted version of the password
+  return bcrypt.compare(candidatePwd, this.password)
+}
+
+User.prototype.generateToken = function() {
+  return jwt.sign({id: this.id}, process.env.JWT)
+}
+
+// classMethods
+User.authenticate = async function({ username, password }) {
+  const user = await this.findOne({ where: { username } })
+  if(!user || !(await user.correctPassword(password))) {
+    const error = Error('Incorrect username/password');
+    error.status = 401;
+    throw error;
+  }
+  return user.generateToken();
+};
+
+User.findByToken = async function(token) {
+  try {
+    const {id} = await jwt.verify(token, process.env.JWT)
+    const user = User.findByPk(id)
+    if(!user) {
+      throw 'noooooooo'
+    }
+  } catch(e) {
+    const error = Error('bad token')
+    error.status = 401;
+    throw error;
+  }
+}
+
+// hooks
+const hashPassword = async (user) => {
+  //in case the password has been changed, we want to encrypt it with bcrypt
+  if(user.changed('password')) {
+    user.password = await bcrypt.hash(user.password, SALT_ROUNDS);
+  }
+}
